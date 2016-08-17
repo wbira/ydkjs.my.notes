fb4a6b79a0fe3f69b3f8d7b9fd6537a296961ab2
Waldemar Bira <waldek.bira@gmail.com>
2016-08-17 23:43:30 +0200

    added missing diff between LHS and RHS

--- course/scope/example1.js
+++ course/scope/example1.js
@@ -7,7 +7,12 @@ function bar()  {
 function baz(foo){
     foo = "bam";
     bam = "yay";
+    console.log(b);
 }
+
+bar();
+
+baz("qux")
 
 
 /*
@@ -30,7 +35,8 @@ EXECUTION PHASE:
 Durring execution phase all declarations (variables and function) are going to dissapear.
 
 LHS => left hand side of equal operator (target, where we put soruce)
-RHS => right hand side of equal operator (source)
+RHS => right hand side of equal operator, or even bettet not left hand side
+ (retrieve source, get value)
 
 Starting execution
 
@@ -39,5 +45,20 @@ if not, it will try find in in another scope).
 Yes it is, so scope will return a reference to foo and then we can assign "bar" statment to foo. 
 (If the right would be something more complicated, e.g variable engine will ask scope about RHS reference)
 
-TODO diff between LHS and RHS and diff between declaration
+After function bar engine will try execute baz. It will be almost the same like in bar, except line 9.
+In line 9 engine will ask:
+E:"Hey scope of baz, do you have LHS reference to bam"
+S:"No, never heard"
+E:"Hey global scope, do you know something about bam?
+S:"No, but I will create global variable bam for you (THIS IS BAD)
+
+DIFF between LHS and RHS
+1. RHS => if engine will perform RHS lookup e.g variable b, and it will fails (including global and all nested scopes) engine will throw REFERENCE ERROR
+2. LHS => if LHS lookup fails there are two cases:
+    a.) program is running in non strict mode => global scope will create new variable of the name
+    b.) program is running in strict mode => engine will trhow REFERENCE ERROR like in RHS
+
+Type Error is when enginge try execute-as-function a non-function value, or reference a property on a null or undefined value
+
+So Reference Error is related to scope issue, TypeError is a illegal operation
 */
