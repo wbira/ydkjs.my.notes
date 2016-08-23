3246cdad62a8027fd1ee3e680c29136c49593020
Waldemar Bira <waldek.bira@gmail.com>
2016-08-23 22:05:42 +0200

    added notes about let keyword

---
+++ course/scope/blockScope.js
@@ -0,0 +1,20 @@
+/*
+We use let keywor for declare variable in block scope. It can be easily use with for loop or in if block e.g 
+We need only pair of braces.
+
+*/
+
+function foo() {
+    var bar = "bar";
+    for(let i=0; i <bar.length; i++){
+        console.log(bar.charAt(i));
+    }
+    console.log(i) //will throw reference error
+}
+
+
+/* 
+Issues with let
+- let is not hoisted
+
+*/
