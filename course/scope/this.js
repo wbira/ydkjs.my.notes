56b99d5d2e17f0aee3920c85b35daf19eee45e3e
Waldemar Bira <waldek.bira@gmail.com>
2016-08-25 22:42:46 +0200

    added notest abot implicite binding

--- course/scope/this.js
+++ course/scope/this.js
@@ -40,3 +40,21 @@ foo()
 })()
 
 var bar = "bar1";
+
+/* Implicit binding rule 
+this rule is applied when function is a called as
+a property of anther object, and these object is
+an executuion context for our funstion
+
+*/
+
+function foo (){
+    'use strict'
+    console.log(this.bar);
+}
+
+var o2 = { bar : "bar2", foo: foo }
+var o2 = { bar : "bar3", foo: foo }
+
+o2.foo(); //bar2
+o3.foo(); //bar3
