170b3c132f3de60cf65c97bdffa27441462aa7e9
Waldemar Bira <waldek.bira@gmail.com>
2016-10-03 18:44:18 +0200

    addet notes about new keyword

---
+++ course/objects/new.js
@@ -0,0 +1,12 @@
+/****** new keyword *******/
+
+/*
+Constructor in JS is just a normal function.
+When function is invoked with kew keyword at the beginning, there are four things happening:
+
+1. new object o is created
+2. o is linked to the prototype
+3. o is this for this function call
+4. invoked function return o (unless function returns own alternate object)
+
+ */
